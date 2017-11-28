7f49d34f1c2356ac91ee04255a1cfdde458835cd
hss1994 <1070798770@qq.com>
2017-11-28 22:24:15 +0800

    update

--- static/js/app/controller/share/ShareUpload.js
+++ static/js/app/controller/share/ShareUpload.js
@@ -8,49 +8,46 @@ define([
     init();
     
     function init() {
-    	$("#upload_android").click(function(){
-    		getAndroidUrl();
-    	})
     	
-    	$("#upload_ios").click(function(){
-    		getIosUrl();
-    	})
-    	
+		base.showLoading("加载中...")
+		$.when(
+			getAndroidUrl(),
+			getIosUrl()
+		).then(function(){
+			base.hideLoading()
+		})
+		
     }
 	
 	function getAndroidUrl(){
-		base.showLoading("加载中...")
-		Ajax.get("625918",{
+		return Ajax.get("625918",{
 			"type":"android-c",
 			"systemCode":SYSTEM_CODE,
 			"companyCode":SYSTEM_CODE
 		}).then(function(res) {
-			base.hideLoading()
 	        if (res.success) {
-        		window.location.href=res.data.downloadUrl;
+        		$("#upload_android").attr("href",res.data.downloadUrl);
 	        } else {
 	        	base.showMsg(res.msg);
 	        }
 	    }, function() {
-	        base.showMsg("获取下载地址失败");
+	        base.showMsg("获取安卓下载地址失败");
 	    });
 	}
 	
 	function getIosUrl(){
-		base.showLoading("加载中...")
-		Ajax.get("625918",{
+		return Ajax.get("625918",{
 			"type":"ios-c",
 			"systemCode":SYSTEM_CODE,
 			"companyCode":SYSTEM_CODE
 		}).then(function(res) {
-			base.hideLoading()
 	        if (res.success) {
-        		window.location.href=res.data.downloadUrl;
+        		$("#upload_ios").attr("href",res.data.downloadUrl);
 	        } else {
 	        	base.showMsg(res.msg);
 	        }
 	    }, function() {
-	        base.showMsg("获取下载地址失败");
+	        base.showMsg("获取ios下载地址失败");
 	    });
 	}
 });
