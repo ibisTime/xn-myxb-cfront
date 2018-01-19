11a3957ed8ac5a77452308910d67e4e474a3d439
clare29 <1397452342@qq.com>
2018-01-19 17:06:35 +0800

    updata

--- static/js/app/controller/eth/nodeinfo.js
+++ static/js/app/controller/eth/nodeinfo.js
@@ -6,9 +6,7 @@ define([
     init();
     
     function init() {
-    	$('.upload-logo').css('background','rgb(241,83,83)').css('margin-top','0');
-    	$('.uploadWrap').css('margin-top','0.5rem').css('padding','2.5rem 0');
-        $('.uploadWrap input').css('text-align','left').css('padding-left','0.3rem').css('font-size','0.3rem')
+
 		base.showLoading("加载中...")
 		$.when(
 			getNodeinfo()
@@ -23,10 +21,10 @@ define([
         return Ajax.get("625800", {}).then(function (data) {
             var list = data.data;
 			$('#bcoinBlockNumber').attr('value','Bcoin最大区块号: '+list.bcoinBlockNumber)
-			$('#bcoinGasPrice').attr('value','Bcoin最新Gas价格: '+list.bcoinGasPrice)
+			$('#bcoinGasPrice').attr('value','Bcoin最新Gas价格: '+list.bcoinGasPrice+' WEI')
 			$('#bcoinScaned').attr('value','Bcoin当前扫描区块号: '+list.bcoinScaned)
 			$('#infuraBlockNumber').attr('value','Infrua最大区块号: '+list.infuraBlockNumber)
-			$('#infuraGasPrice').attr('value','Infrua最新Gas价格: '+list.infuraGasPrice)
+			$('#infuraGasPrice').attr('value','Infrua最新Gas价格: '+list.infuraGasPrice+' WEI')
         })
     }
 
