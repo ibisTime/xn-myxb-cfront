9d9fab15d0c303c7c32df81f66c577b13883778c
hss1994 <1070798770@qq.com>
2018-05-11 16:00:13 +0800

    updata

--- static/js/app/config.js
+++ static/js/app/config.js
@@ -1,6 +1,6 @@
 var SYSTEM_CODE = "CD-CXB000020";
 var COMPANY_CODE = "CD-CXB000020";
-var PIC_PREFIX = 'http://otoieuivb.bkt.clouddn.com';
+var PIC_PREFIX = 'http://p8i9tvzga.bkt.clouddn.com';
 var THUMBNAIL_SUFFIX = '?imageView2/1/w/750/h/420';
 var THUMBNAIL_SUFFIX2 = '?imageMogr2/auto-orient/thumbnail/!375x210r';
 var THUMBNAIL_INDEX = '?imageView2/1/w/80/h/80';
